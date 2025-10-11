d11f6071caf0180ba184c05b98b3f10548266a66
Novimetrix <mohamed.elmalik@gmail.com>
2025-10-11 16:53:03 +0400

    as

--- run-goc-seo-minimal.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-title GOC Minimal SEO Patch (WSH)
-echo === GOC Minimal SEO Patch (WSH) ===
-cd /d "%~dp0"
-cscript //nologo goc-seo-minimal-wsh.js
-echo.
-echo Finished.
-pause
---
+++ run-goc-seo-perf-wsh.cmd
@@ -0,0 +1,8 @@
+@echo off
+title GOC SEO Perf Patch (WSH)
+echo === GOC SEO Perf Patch (WSH) ===
+cd /d "%~dp0"
+cscript //nologo goc-seo-perf-wsh.js
+echo.
+echo Finished.
+pause
