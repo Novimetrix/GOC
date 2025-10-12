eed815deb0db9a38dc236610673b71ebf054652c
Novimetrix <mohamed.elmalik@gmail.com>
2025-10-12 17:08:59 +0400

    3265

---
+++ assets/js/nm-sticky.js
@@ -0,0 +1,11 @@
+﻿(function(){
+  function setPad(){
+    var h = document.querySelector('.ct-header, header.site-header');
+    if(!h) return;
+    var hh = h.getBoundingClientRect().height || 64;
+    document.body.style.paddingTop = hh + 'px';
+  }
+  setPad();
+  window.addEventListener('resize', function(){ setPad(); }, {passive:true});
+  document.addEventListener('DOMContentLoaded', setPad);
+})();
